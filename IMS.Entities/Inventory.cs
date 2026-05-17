2809126a3800bfca86f8436492dec936087ab7b5
MRFekry <mfekry.ra@gmail.com>
2026-05-17 20:53:04 +0200

    Introduces validation attributes for inventory entity

--- IMS.Entities/Inventory.cs
+++ IMS.Entities/Inventory.cs
@@ -1,9 +1,18 @@
-﻿namespace IMS.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.Entities;
 
 public class Inventory
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required.")]
+    [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
     public string Name { get; set; } = string.Empty;
+
+    [Range(0, int.MaxValue, ErrorMessage = "Quantity must be a non-negative integer.")]
     public int Quantity { get; set; }
+
+    [Range(0, double.MaxValue, ErrorMessage = "Price must be a non-negative number.")]
     public decimal Price { get; set; }
 }
