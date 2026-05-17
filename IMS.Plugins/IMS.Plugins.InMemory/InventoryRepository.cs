1241ec8180f5d196d61b10a4c7f0e8e7a1484fe8
MRFekry <mfekry.ra@gmail.com>
2026-05-17 20:53:04 +0200

    Adds inventory creation logic to repository and use case layer

--- IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -43,7 +43,22 @@ public class InventoryRepository : IInventoryRepository
         return Task.FromResult(
             _inventories.Where(
                 i => i.Name.Contains(nameFilter, StringComparison.OrdinalIgnoreCase)
-            )
+            ).ToList()
         );
+    }
+
+    public Task AddInventoryAsync(Inventory inventory)
+    {
+        if (_inventories.Any(i => i.Name.Equals(inventory.Name, StringComparison.OrdinalIgnoreCase)))
+        {
+            return Task.CompletedTask; // Inventory with the same name already exists, do not add
+        }
+
+        var maxId = _inventories.Any() ? _inventories.Max(i => i.Id) : 0;
+        inventory.Id = maxId + 1; // Assign a new unique ID to the inventory
+
+        // Add the new inventory (implementation for adding inventory goes here)
+        _inventories.Add(inventory);
+        return Task.CompletedTask;
     }
 }
---
+++ IMS.UseCases/UseCases/Inventory/AddNewInventoryUseCase.cs
@@ -0,0 +1,23 @@
+﻿using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases.UseCases.Inventory;
+
+public interface IAddNewInventoryUseCase
+{
+    Task ExecuteAsync(Entities.Inventory inventory);
+}
+
+public class AddNewInventoryUseCase : IAddNewInventoryUseCase
+{
+    private readonly IInventoryRepository _inventoryRepository;
+
+    public AddNewInventoryUseCase(IInventoryRepository inventoryRepository)
+    {
+        _inventoryRepository = inventoryRepository;
+    }
+
+    public async Task ExecuteAsync(Entities.Inventory inventory)
+    {
+        await _inventoryRepository.AddInventoryAsync(inventory);
+    }
+}
